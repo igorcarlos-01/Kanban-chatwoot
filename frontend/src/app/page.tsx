773b60b0cac0c73d8461c5ee9544a97a36780f61
igorcarlos-01 <igor.flu99@gmail.com>
2026-04-07 23:55:32 +0000

    Add files via upload

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,11 +4,11 @@ import KanbanBoard from "@/components/KanbanBoard";
 
 export default function Home() {
   return (
-    <div className="h-full p-6">
+    <div className="h-full p-2 md:p-6">
       <div className="mb-6 flex justify-between items-center">
-        <h2 className="text-2xl font-bold text-gray-800">Sales Pipeline</h2>
-        <button className="bg-brand text-white px-4 py-2 rounded-md shadow hover:bg-sky-600 transition">
-          + Add Lead
+        <h2 className="text-xl md:text-2xl font-bold text-gray-800">Seu Funil de Vendas</h2>
+        <button className="bg-blue-600 text-white px-3 md:px-4 py-2 text-sm md:text-base rounded-md shadow hover:bg-blue-700 transition font-bold" onClick={() => alert("Essa função vai abrir uma janela para adicionar contatos manualmente!")}>
+          + Novo Lead
         </button>
       </div>
       <div className="h-[calc(100%-80px)]">
